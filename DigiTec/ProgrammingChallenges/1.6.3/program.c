f99cd2bfb8984243e9dbf4e530d2f061dd627081
Justin Rogers <justrog@gmail.com>
2013-09-21 17:44:14 -0700

    Added debugging support to interrogate the rounding errors in double/float usage.
    Fixed rounding with an EPSILON check that now passes input3.txt. The extra debugging output helps to identify that I'm landing on the right answer correctly as well.

--- DigiTec/ProgrammingChallenges/1.6.3/program.c
+++ DigiTec/ProgrammingChallenges/1.6.3/program.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <memory.h>
+#include <math.h>
+
+#define EPSILON 0.00000000001
+int debugMode = 0;
 
 void ProcessTrip(unsigned long students);
-int main()
+int main(int argc)
 {
     unsigned long students;
+
+    if (argc > 1)
+    {
+        printf("Debug Mode...\n");
+        debugMode = 1;
+    }
+
     fscanf(stdin, "%d\n", &students);
     while (students > 0)
     {
@@ -21,6 +32,7 @@ void ProcessTrip(unsigned long students)
     double amtAvg = 0;
     double negDiff = 0;
     double posDiff = 0;
+    double acceptedDiff;
 
     unsigned long amtIter;
     for (amtIter = 0; amtIter < students; amtIter++)
@@ -30,18 +42,51 @@ void ProcessTrip(unsigned long students)
     }
 
     amtAvg = amtTotal / students;
+    if (debugMode)
+    {
+        printf("Target Average: %lf\n", amtAvg);
+    }
     for (amtIter = 0; amtIter < students; amtIter++)
     {
         // Trickiness here is casting to ensure roundness to "cents" in the numbers.
-        double diff = (double) (long) ((studentSpend[amtIter] - amtAvg) * 100) / 100;
+        // double diff = (double) (long) ((studentSpend[amtIter] - amtAvg) * 100) / 100;
+        double diff = (double) (long) ((studentSpend[amtIter] - amtAvg) * 100 + EPSILON) / 100;
+
+        if (debugMode)
+        {
+            printf("Computational Diff: %lf\n", studentSpend[amtIter] - amtAvg);
+            printf("Computational Diff 2: %lf\n", (studentSpend[amtIter] - amtAvg) * 100);
+            printf("Computational Diff 3: %lf\n", (studentSpend[amtIter] - amtAvg) * 100 / 100);
+            printf("Computational Diff 4: %lu\n", (unsigned long) ((studentSpend[amtIter] - amtAvg) * 100));
+        }
+
         if (diff > 0)
         {
+            if (debugMode)
+            {
+                printf("Positive Diff: %lf for spend %lf\n", diff, studentSpend[amtIter]);
+            }
             posDiff += diff;
         }
         else
         {
+            if (debugMode)
+            {
+                printf("Negative Diff: %lf for spend %lf\n", diff, studentSpend[amtIter]);
+            }
             negDiff += diff;
         }
     }
-    printf("$%.2lf\n", (-negDiff > posDiff) ? -negDiff : posDiff);
+
+    if (debugMode)
+    {
+        printf("Diffs: neg($%.2lf), pos($%.2lf)\n", negDiff, posDiff);
+    }
+
+    acceptedDiff = posDiff;
+    if (-negDiff > posDiff)
+    {
+        acceptedDiff = -negDiff;
+    }
+    printf("$%.2lf\n", acceptedDiff);
 }
