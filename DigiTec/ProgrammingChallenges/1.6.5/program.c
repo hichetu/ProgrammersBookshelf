aea525777d32e1654b43053f38e4c68c01497f90
Justin Rogers <justrog@gmail.com>
2013-09-22 17:30:32 -0700

    This change fixes my input handling yet again. Turns out sscanf and scanf can both leave garbage in the upper values, so I've gone ahead and initialized everything and then used the right format specifiers. I also think one of the tests is bogus and the call to scanf actually fails but you are expected to use a "default value" of sorts. This is crap and I'm only about half certain this is the case, but initializing variables isn't terrible.
    I've also added a performance improvement to Fill that avoids an extra GetPixel/Compare call by doing this in the main call, but then doing the remaining fill pixels in a worker.

--- DigiTec/ProgrammingChallenges/1.6.5/program.c
+++ DigiTec/ProgrammingChallenges/1.6.5/program.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <memory.h>
 
-int debugMode = 0;
 char bitmap[250][250];
 unsigned long width;
 unsigned long height;
@@ -12,17 +11,14 @@ void SetPixel(unsigned long x, unsigned long y, char color);
 void DrawRectangle(unsigned long x1, unsigned long x2, unsigned long y1, unsigned long y2, char color);
 void SaveBitmap(char* szFile);
 void Fill(unsigned long x, unsigned long y, char color);
+void FillWorker(unsigned long x, unsigned long y, char color, char oldColor);
 void SwapMax(unsigned long* ul1, unsigned long* ul2);
 
 int main(int argc)
 {
-    if (argc > 1)
-    {
-        debugMode = 1;
-    }
-
     do
     {
+        char targetCommand;
         char commandLine[1024];
         char* command = gets(commandLine);
 
@@ -31,19 +27,11 @@ int main(int argc)
             break;
         }
 
-        if (debugMode)
-        {
-            printf("Command %c\n", command[0]);
-        }
-
-        switch (command[0])
+        sscanf(command, "%c", &targetCommand);
+        switch (targetCommand)
         {
             case 'I':
-                sscanf(&command[1], "%d %d", &width, &height);
-                if (debugMode)
-                {
-                    printf("I with %d %d\n", width, height);
-                }
+                sscanf(&command[1], "%c %lu %lu", &targetCommand, &width, &height);
                 ClearImage();
                 break;
 
@@ -53,25 +41,21 @@ int main(int argc)
 
             case 'L':
                 {
-                    unsigned long x;
-                    unsigned long y;
-                    char color;
-                    sscanf(&command[1], "%d %d %c", &x, &y, &color);
-                    if (debugMode)
-                    {
-                        printf("L with %d %d %c\n", x, y, color);
-                    }
+                    unsigned long x = 0;
+                    unsigned long y = 0;
+                    char color = 0;
+                    sscanf(&command[1], "%c %lu %lu %c", &targetCommand, &x, &y, &color);
                     SetPixel(x, y, color);
                 }
                 break;
 
             case 'V':
                 {
-                    unsigned long x;
-                    unsigned long y1;
-                    unsigned long y2;
-                    char color;
-                    sscanf(&command[1], "%d %d %d %c", &x, &y1, &y2, &color);
+                    unsigned long x = 0;
+                    unsigned long y1 = 0;
+                    unsigned long y2 = 0;
+                    char color = 0;
+                    sscanf(&command[1], "%c %lu %lu %lu %c", &targetCommand, &x, &y1, &y2, &color);
                     SwapMax(&y1, &y2);
                     DrawRectangle(x, x, y1, y2, color);
                 }
@@ -79,11 +63,11 @@ int main(int argc)
 
             case 'H':
                 {
-                    unsigned long x1;
-                    unsigned long x2;
-                    unsigned long y;
-                    char color;
-                    sscanf(&command[1], "%d %d %d %c", &x1, &x2, &y, &color);
+                    unsigned long x1 = 0;
+                    unsigned long x2 = 0;
+                    unsigned long y = 0;
+                    char color = 0;
+                    sscanf(&command[1], "%c %lu %lu %lu %c", &targetCommand, &x1, &x2, &y, &color);
                     SwapMax(&x1, &x2);
                     DrawRectangle(x1, x2, y, y, color);
                 }
@@ -91,12 +75,12 @@ int main(int argc)
 
             case 'K':
                 {
-                    unsigned long x1;
-                    unsigned long x2;
-                    unsigned long y1;
-                    unsigned long y2;
-                    char color;
-                    sscanf(&command[1], "%d %d %d %d %c", &x1, &y1, &x2, &y2, &color);
+                    unsigned long x1 = 0;
+                    unsigned long x2 = 0;
+                    unsigned long y1 = 0;
+                    unsigned long y2 = 0;
+                    char color = 0;
+                    sscanf(&command[1], "%c %lu %lu %lu %lu %c", &targetCommand, &x1, &y1, &x2, &y2, &color);
                     SwapMax(&x1, &x2);
                     SwapMax(&y1, &y2);
                     DrawRectangle(x1, x2, y1, y2, color);
@@ -105,10 +89,10 @@ int main(int argc)
 
             case 'F':
                 {
-                    unsigned long x;
-                    unsigned long y;
-                    char color;
-                    sscanf(&command[1], "%d %d %c", &x, &y, &color);
+                    unsigned long x = 0;
+                    unsigned long y = 0;
+                    char color = 0;
+                    sscanf(&command[1], "%c %lu %lu %c", &targetCommand, &x, &y, &color);
                     Fill(x, y, color);
                 }
                 break;
@@ -116,7 +100,7 @@ int main(int argc)
             case 'S':
                 {
                     char achFile[1024];
-                    sscanf(&command[1], "%s", &achFile);
+                    sscanf(&command[1], "%c %s", &targetCommand, &achFile);
                     SaveBitmap(achFile);
                 }
                 break;
@@ -200,28 +184,33 @@ void SaveBitmap(char* szFile)
 
 void Fill(unsigned long x, unsigned long y, char color)
 {
-    char colorSrc = GetPixel(x, y);
+    char oldColor = GetPixel(x, y);
 
     // If we fill the same color, then that would be a no-op
-    if (colorSrc != color)
+    if (oldColor != color)
     {
-        SetPixel(x, y, color);
-        if (ComparePixel(x, y-1, colorSrc))
-        {
-            Fill(x, y-1, color);
-        }
-        if (ComparePixel(x, y+1, colorSrc))
-        {
-            Fill(x, y+1, color);
-        }
-        if (ComparePixel(x-1, y, colorSrc))
-        {
-            Fill(x-1, y, color);
-        }
-        if (ComparePixel(x+1, y, colorSrc))
-        {
-            Fill(x+1, y, color);
-        }
+        FillWorker(x, y, color, oldColor);
+    }
+}
+
+void FillWorker(unsigned long x, unsigned long y, char color, char oldColor)
+{
+    SetPixel(x, y, color);
+    if (ComparePixel(x, y-1, oldColor))
+    {
+        FillWorker(x, y-1, color, oldColor);
+    }
+    if (ComparePixel(x, y+1, oldColor))
+    {
+        FillWorker(x, y+1, color, oldColor);
+    }
+    if (ComparePixel(x-1, y, oldColor))
+    {
+        FillWorker(x-1, y, color, oldColor);
+    }
+    if (ComparePixel(x+1, y, oldColor))
+    {
+        FillWorker(x+1, y, color, oldColor);
     }
 }
 
