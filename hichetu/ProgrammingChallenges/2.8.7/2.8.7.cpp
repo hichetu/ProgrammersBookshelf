e3a881ebdaf109e4305d3becb7fe1fa38f8ca35c
hichetu <hichetu@gmail.com>
2013-11-07 18:10:59 -0800

    2.8.7 V02

--- hichetu/ProgrammingChallenges/2.8.7/2.8.7.cpp
+++ hichetu/ProgrammingChallenges/2.8.7/2.8.7.cpp
@@ -1,11 +1,90 @@
-// 2.8.7.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+// 2.8.7.cpp : http://www.programming-challenges.com/pg.php?page=downloadproblem&probid=110207&format=html
 
 
-int _tmain(int argc, _TCHAR* argv[])
+class Problem
 {
-	return 0;
-}
+public:
+	enum Status
+	{
+		CORRECT,
+		INCORRECT,
+		UNSOLVED,
+	};
+
+public:
+
+	Problem() : 
+		status_(UNSOLVED),
+		solvedTime_(0),
+		incorrectSubmissions_(0)
+	{
+	}
+
+
+	Status& ProblemStatus()
+	{
+		return status_;
+	}
+
+	int& SolvedTime()
+	{
+		return solvedTime_;
+	}
+
+	const int& NumberOfIncorrectSubmissions() const
+	{
+		return incorrectSubmissions_;
+	}
+
+	void IncrementIncorrectSubmissions()
+	{
+		++incorrectSubmissions_;
+	}
+
+private:
+
+	Status status_;
+	int solvedTime_;
+	int incorrectSubmissions_;
+};
+
+
+class Contestant
+{
+public:
+
+	Contestant(int number) :
+		number_(number)
+	{
+
+	}
+
+	const int& Number() const
+	{
+		return number_;
+	}
+
+	const int& Penalty() const
+	{
+		return penalty_;
+	}
+
+	const int& Solved() const
+	{
+		return solved_;
+	}
+
+	void Evaluate(int problemNumber, int time, Problem::Status status)
+	{
+
+	}
+
+private:
+
+	int number_;
+	int penalty_;
+	int solved_;
+
+	Problem problems_[10];
+};
 
