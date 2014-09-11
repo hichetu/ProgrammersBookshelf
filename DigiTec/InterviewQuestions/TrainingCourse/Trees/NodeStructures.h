68701e5c8a5251c33ea35ca179b8efab51fdede5
Justin Rogers <justrog@gmail.com>
2014-09-10 23:10:11 -0700

    Fix tabs to spaces.
    Check in the FindNode BST search function (recursive version)

--- DigiTec/InterviewQuestions/TrainingCourse/Trees/NodeStructures.h
+++ DigiTec/InterviewQuestions/TrainingCourse/Trees/NodeStructures.h
@@ -2,172 +2,172 @@ template <typename TRoot>
 class Tree
 {
 public:
-	Tree(TRoot* root) : m_root(root)
-	{
-	}
+    Tree(TRoot* root) : m_root(root)
+    {
+    }
 
-	TRoot* Root()
-	{
-		return m_root;
-	}
+    TRoot* Root()
+    {
+        return m_root;
+    }
 
 private:
-	TRoot* m_root;
+    TRoot* m_root;
 };
 
 template <typename TValue>
 class FullNode
 {
 public:
-	FullNode(FullNode<TValue>* parent) : 
-		m_value(TValue()), m_parent(parent), 
-		m_left(nullptr), m_right(nullptr)
-	{
-	}
+    FullNode(FullNode<TValue>* parent) :
+        m_value(TValue()), m_parent(parent),
+        m_left(nullptr), m_right(nullptr)
+    {
+    }
 
-	void SetValue(const TValue& value)
-	{
-		m_value = value;
-	}
-	TValue GetValue()
-	{
-		return m_value;
-	}
-	void SetParent(FullNode<TValue>* node)
-	{
-		m_parent = node;
-	}
-	FullNode<TValue>* GetParent()
-	{
-		return m_parent;
-	}
-	void SetLeft(FullNode<TValue>* node)
-	{
-		m_left = node;
-	}
-	FullNode<TValue>* GetLeft()
-	{
-		return m_left;
-	}
-	void SetRight(FullNode<TValue>* node)
-	{
-		m_right = node;
-	}
-	FullNode<TValue>* GetRight()
-	{
-		return m_right;
-	}
+    void SetValue(const TValue& value)
+    {
+        m_value = value;
+    }
+    TValue GetValue()
+    {
+        return m_value;
+    }
+    void SetParent(FullNode<TValue>* node)
+    {
+        m_parent = node;
+    }
+    FullNode<TValue>* GetParent()
+    {
+        return m_parent;
+    }
+    void SetLeft(FullNode<TValue>* node)
+    {
+        m_left = node;
+    }
+    FullNode<TValue>* GetLeft()
+    {
+        return m_left;
+    }
+    void SetRight(FullNode<TValue>* node)
+    {
+        m_right = node;
+    }
+    FullNode<TValue>* GetRight()
+    {
+        return m_right;
+    }
 
 private:
-	TValue m_value;
-	FullNode<TValue>* m_parent;
-	FullNode<TValue>* m_left;
-	FullNode<TValue>* m_right;
+    TValue m_value;
+    FullNode<TValue>* m_parent;
+    FullNode<TValue>* m_left;
+    FullNode<TValue>* m_right;
 };
 
 template <typename TValue>
 class TraversalNode
 {
 public:
-	TraversalNode() : m_value(TValue()), 
-		m_left(nullptr), m_right(nullptr)
-	{
-	}
+    TraversalNode() : m_value(TValue()),
+        m_left(nullptr), m_right(nullptr)
+    {
+    }
 
 private:
-	TValue m_value;
-	TraversalNode<TValue>* m_left;
-	TraversalNode<TValue>* m_right;
+    TValue m_value;
+    TraversalNode<TValue>* m_left;
+    TraversalNode<TValue>* m_right;
 };
 
 template <typename TValue, unsigned long TMAry>
 class MAryNode
 {
 public:
-	MAryNode() : m_value(TValue())
-	{
-		memset(m_children, 0, sizeof(m_children));
-	};
+    MAryNode() : m_value(TValue())
+    {
+        memset(m_children, 0, sizeof(m_children));
+    };
 
 private:
-	TValue m_value;
-	MAryNode<TValue, TMAry>* m_children[TMAry];
+    TValue m_value;
+    MAryNode<TValue, TMAry>* m_children[TMAry];
 };
 
 template <typename TValue>
 class LinkNode
 {
 public:
-	LinkNode() : m_value(TValue()), 
-		m_pNextSibling(nullptr), m_pFirstChild(nullptr)
-	{
-	};
+    LinkNode() : m_value(TValue()),
+        m_pNextSibling(nullptr), m_pFirstChild(nullptr)
+    {
+    };
 
 private:
-	TValue m_value;
-	LinkNode<TValue>* m_pNextSibling;
-	LinkNode<TValue>* m_pFirstChild;
+    TValue m_value;
+    LinkNode<TValue>* m_pNextSibling;
+    LinkNode<TValue>* m_pFirstChild;
 };
 
 template <typename TValue>
 class TrickyNode
 {
 public:
-	TrickyNode() : m_value(TValue()), m_pFirstChild(nullptr), 
-		m_pParentOrNextSibling(nullptr), m_isLeftChild(false), 
-		m_hasNextSibling(false)
-	{
-	}
-	
-	bool isRoot()
-	{
-		// For this tree, you either point to your 
-		// parent or sibling, unless you are the root
-		return (m_pParentOrNextSibling == nullptr);
-	}
+    TrickyNode() : m_value(TValue()), m_pFirstChild(nullptr),
+        m_pParentOrNextSibling(nullptr), m_isLeftChild(false),
+        m_hasNextSibling(false)
+    {
+    }
 
-	TrickyNode<TValue>* LeftChild() const
-	{
-		if (m_pFirstChild != nullptr && m_pFirstChild->m_isLeftChild)
-		{
-			return m_pFirstChild;
-		}
-		return nullptr;
-	}
+    bool isRoot()
+    {
+        // For this tree, you either point to your 
+        // parent or sibling, unless you are the root
+        return (m_pParentOrNextSibling == nullptr);
+    }
 
-	TrickyNode<TValue>* RightChild() const
-	{
-		if (m_pFirstChild != nullptr)
-		{
-			if (!m_pFirstChild->m_isLeftChild)
-			{
-				return m_pFirstChild;
-			}
-			else if (!m_pFirstChild->m_hasNextSibling)
-			{
-				m_pFirstChild->m_pParentOrNextSibling;
-			}
-		}
-		return nullptr;
-	}
+    TrickyNode<TValue>* LeftChild() const
+    {
+        if (m_pFirstChild != nullptr && m_pFirstChild->m_isLeftChild)
+        {
+            return m_pFirstChild;
+        }
+        return nullptr;
+    }
 
-	TrickyNode<TValue>* Parent() const
-	{
-		if (!m_pParentOrSibling || !m_hasNextSibling)
-		{
-			return m_pParentOrSibling;
-		}
-		else
-		{
-			return m_pParentOrSibling->m_pParentOrSibling;
-		}
-	}
+    TrickyNode<TValue>* RightChild() const
+    {
+        if (m_pFirstChild != nullptr)
+        {
+            if (!m_pFirstChild->m_isLeftChild)
+            {
+                return m_pFirstChild;
+            }
+            else if (!m_pFirstChild->m_hasNextSibling)
+            {
+                m_pFirstChild->m_pParentOrNextSibling;
+            }
+        }
+        return nullptr;
+    }
+
+    TrickyNode<TValue>* Parent() const
+    {
+        if (!m_pParentOrSibling || !m_hasNextSibling)
+        {
+            return m_pParentOrSibling;
+        }
+        else
+        {
+            return m_pParentOrSibling->m_pParentOrSibling;
+        }
+    }
 
 private:
-	TValue m_value;
-	TrickyNode<TValue>* m_pFirstChild;
-	TrickyNode<TValue>* m_pParentOrNextSibling;
-	bool m_isLeftChild;
-	bool m_hasNextSibling;
+    TValue m_value;
+    TrickyNode<TValue>* m_pFirstChild;
+    TrickyNode<TValue>* m_pParentOrNextSibling;
+    bool m_isLeftChild;
+    bool m_hasNextSibling;
 };
 
--- DigiTec/InterviewQuestions/TrainingCourse/Trees/Trees.cpp
+++ DigiTec/InterviewQuestions/TrainingCourse/Trees/Trees.cpp
@@ -7,119 +7,119 @@ using namespace std;
 
 Tree<FullNode<int>>* BuildTreeFromParts(wchar_t* treeParts)
 {
-	vector<FullNode<int>*> nodes;
-	wchar_t* ctx = nullptr;
-	wchar_t* nodeLink = wcstok_s(treeParts, L",", &ctx);
-	while (nodeLink != nullptr)
-	{
-		wchar_t* ctx2 = nullptr;
-		wchar_t* part = wcstok_s(nodeLink, L"-", &ctx2);
+    vector<FullNode<int>*> nodes;
+    wchar_t* ctx = nullptr;
+    wchar_t* nodeLink = wcstok_s(treeParts, L",", &ctx);
+    while (nodeLink != nullptr)
+    {
+        wchar_t* ctx2 = nullptr;
+        wchar_t* part = wcstok_s(nodeLink, L"-", &ctx2);
 
-		int nodeId = _wtoi(part);
-		if (nodes.size() <= nodeId)
-		{
-			nodes.push_back(new FullNode<int>(nullptr));
-			nodes.back()->SetValue(nodeId);
-		}
-		FullNode<int>* parent = nodes[nodeId];
+        int nodeId = _wtoi(part);
+        if (nodes.size() <= nodeId)
+        {
+            nodes.push_back(new FullNode<int>(nullptr));
+            nodes.back()->SetValue(nodeId);
+        }
+        FullNode<int>* parent = nodes[nodeId];
 
-		bool isLeft = false;
-		part = wcstok_s(nullptr, L"-", &ctx2);
-		switch (part[0])
-		{
-			case L'l':
-				isLeft = true;
-				break;
-			case L'r':
-				isLeft = false;
-				break;
-			default:
-				_ASSERT(false);
-				break;
-		}
-		part++;
+        bool isLeft = false;
+        part = wcstok_s(nullptr, L"-", &ctx2);
+        switch (part[0])
+        {
+        case L'l':
+            isLeft = true;
+            break;
+        case L'r':
+            isLeft = false;
+            break;
+        default:
+            _ASSERT(false);
+            break;
+        }
+        part++;
 
-		int childId = _wtoi(part);
-		if (nodes.size() <= childId)
-		{
-			nodes.push_back(new FullNode<int>(nullptr));
-			nodes.back()->SetValue(childId);
-		}
-		FullNode<int>* child = nodes[childId];
-		child->SetParent(parent);
-		if (isLeft)
-		{
-			parent->SetLeft(child);
-		}
-		else
-		{
-			parent->SetRight(child);
-		}
-		// Finish the enumeration so we make sure the string is in the right state
-		part = wcstok_s(nullptr, L"-", &ctx2);
-		_ASSERT(part == nullptr);
+        int childId = _wtoi(part);
+        if (nodes.size() <= childId)
+        {
+            nodes.push_back(new FullNode<int>(nullptr));
+            nodes.back()->SetValue(childId);
+        }
+        FullNode<int>* child = nodes[childId];
+        child->SetParent(parent);
+        if (isLeft)
+        {
+            parent->SetLeft(child);
+        }
+        else
+        {
+            parent->SetRight(child);
+        }
+        // Finish the enumeration so we make sure the string is in the right state
+        part = wcstok_s(nullptr, L"-", &ctx2);
+        _ASSERT(part == nullptr);
 
-		nodeLink = wcstok_s(nullptr, L",", &ctx);
-	}
+        nodeLink = wcstok_s(nullptr, L",", &ctx);
+    }
 
-	Tree<FullNode<int>>* newTree = new Tree<FullNode<int>>(nodes[0]);
-	return newTree;
+    Tree<FullNode<int>>* newTree = new Tree<FullNode<int>>(nodes[0]);
+    return newTree;
 }
 
 Tree<FullNode<int>>* TreeConstructor(const wchar_t* treeDefinition)
 {
-	Tree<FullNode<int>>* newTree = nullptr;
-	wchar_t* mutableTreeDefinition = _wcsdup(treeDefinition);
-	wchar_t* ctx = nullptr;
-	
-	vector<wchar_t*> parts;
+    Tree<FullNode<int>>* newTree = nullptr;
+    wchar_t* mutableTreeDefinition = _wcsdup(treeDefinition);
+    wchar_t* ctx = nullptr;
 
-	wchar_t* part = wcstok_s(mutableTreeDefinition, L"\n", &ctx);
-	while (part != nullptr)
-	{
-		parts.push_back(_wcsdup(part));
-		part = wcstok_s(nullptr, L"\n", &ctx);
-	}
+    vector<wchar_t*> parts;
 
-	if (parts.size() > 0)
-	{
-		wchar_t* treePart = parts.front();
-		parts.erase(parts.begin());
+    wchar_t* part = wcstok_s(mutableTreeDefinition, L"\n", &ctx);
+    while (part != nullptr)
+    {
+        parts.push_back(_wcsdup(part));
+        part = wcstok_s(nullptr, L"\n", &ctx);
+    }
 
-		newTree = BuildTreeFromParts(treePart);
-	}
-	return newTree;
+    if (parts.size() > 0)
+    {
+        wchar_t* treePart = parts.front();
+        parts.erase(parts.begin());
+
+        newTree = BuildTreeFromParts(treePart);
+    }
+    return newTree;
 }
 
 void PrintPreOrderRecursive(FullNode<int>* root)
 {
-	if (root != nullptr)
-	{
-		wprintf(L"%d", root->GetValue());
-		PrintPreOrderRecursive(root->GetLeft());
-		PrintPreOrderRecursive(root->GetRight());
-	}
+    if (root != nullptr)
+    {
+        wprintf(L"%d", root->GetValue());
+        PrintPreOrderRecursive(root->GetLeft());
+        PrintPreOrderRecursive(root->GetRight());
+    }
 }
 void PrintPreOrderIterativeStack(FullNode<int>* root)
 {
-	vector<FullNode<int>*> stack;
-	stack.push_back(root);
+    vector<FullNode<int>*> stack;
+    stack.push_back(root);
 
-	while (stack.size() > 0)
-	{
-		FullNode<int>* node = stack.back();
-		stack.erase(stack.end() - 1);
-		
-		wprintf(L"%d", node->GetValue());
-		if (node->GetRight() != nullptr)
-		{
-			stack.push_back(node->GetRight());
-		}
-		if (node->GetLeft() != nullptr)
-		{
-			stack.push_back(node->GetLeft());
-		}
-	}
+    while (stack.size() > 0)
+    {
+        FullNode<int>* node = stack.back();
+        stack.erase(stack.end() - 1);
+
+        wprintf(L"%d", node->GetValue());
+        if (node->GetRight() != nullptr)
+        {
+            stack.push_back(node->GetRight());
+        }
+        if (node->GetLeft() != nullptr)
+        {
+            stack.push_back(node->GetLeft());
+        }
+    }
 }
 void PrintPreOrderParentPointer(FullNode<int>* root)
 {
@@ -161,12 +161,12 @@ void PrintPreOrderParentPointer(FullNode<int>* root)
 
 void PrintInOrderRecursive(FullNode<int>* root)
 {
-	if (root != nullptr)
-	{
-		PrintInOrderRecursive(root->GetLeft());
-		wprintf(L"%d", root->GetValue());
-		PrintInOrderRecursive(root->GetRight());
-	}
+    if (root != nullptr)
+    {
+        PrintInOrderRecursive(root->GetLeft());
+        wprintf(L"%d", root->GetValue());
+        PrintInOrderRecursive(root->GetRight());
+    }
 }
 void PrintInOrderParentPointer(FullNode<int>* root)
 {
@@ -258,7 +258,7 @@ void PrintInOrderParentPointer_Reduced(FullNode<int>* root)
                 prev = nullptr;
             }
         }
-        
+
         prev = current;
         current = current->GetParent();
     }
@@ -266,35 +266,35 @@ void PrintInOrderParentPointer_Reduced(FullNode<int>* root)
 
 void PrintInOrderIterativeStack(FullNode<int>* root)
 {
-	vector<FullNode<int>*> stack;
-	FullNode<int>* current = root;
-	while (stack.size() > 0 || current != nullptr)
-	{
-		// First iterates down the left sub-tree
-		if (current != nullptr)
-		{
-			stack.push_back(current);
-			current = current->GetLeft();
-		}
-		else
-		{
-			// If we get here then our last current didn't have a left sub-tree
-			current = stack.back();
-			stack.erase(stack.end() - 1);
-			wprintf(L"%d", current->GetValue());
-			current = current->GetRight();
-		}
-	}
+    vector<FullNode<int>*> stack;
+    FullNode<int>* current = root;
+    while (stack.size() > 0 || current != nullptr)
+    {
+        // First iterates down the left sub-tree
+        if (current != nullptr)
+        {
+            stack.push_back(current);
+            current = current->GetLeft();
+        }
+        else
+        {
+            // If we get here then our last current didn't have a left sub-tree
+            current = stack.back();
+            stack.erase(stack.end() - 1);
+            wprintf(L"%d", current->GetValue());
+            current = current->GetRight();
+        }
+    }
 }
 
 void PrintPostOrderRecursive(FullNode<int>* root)
 {
-	if (root != nullptr)
-	{
-		PrintPostOrderRecursive(root->GetLeft());
-		PrintPostOrderRecursive(root->GetRight());
-		wprintf(L"%d", root->GetValue());
-	}
+    if (root != nullptr)
+    {
+        PrintPostOrderRecursive(root->GetLeft());
+        PrintPostOrderRecursive(root->GetRight());
+        wprintf(L"%d", root->GetValue());
+    }
 }
 void PrintPostOrderIterativeStack(FullNode<int>* root)
 {
@@ -365,18 +365,39 @@ void PrintPostOrderParentPointer(FullNode<int>* root)
         }
     }
 }
+
+FullNode<int>* FindNode(FullNode<int>* root, int value)
+{
+    if (root == nullptr)
+    {
+        return root;
+    }
+    if (root->GetValue() == value)
+    {
+        return root;
+    }
+    else if (root->GetValue() > value)
+    {
+        return FindNode(root->GetLeft(), value);
+    }
+    else
+    {
+        return FindNode(root->GetRight(), value);
+    }
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Tree<LinkNode<int>> linkTree(new LinkNode<int>());
-	Tree<MAryNode<int, 3>> mAryTree(new MAryNode<int, 3>());
-	Tree<TrickyNode<int>> trickyTree(new TrickyNode<int>());
+    Tree<LinkNode<int>> linkTree(new LinkNode<int>());
+    Tree<MAryNode<int, 3>> mAryTree(new MAryNode<int, 3>());
+    Tree<TrickyNode<int>> trickyTree(new TrickyNode<int>());
 
-	Tree<FullNode<int>>* simpleTree = TreeConstructor(L"0-l1,0-r2,1-l3,1-r4,2-l5,2-r6");
+    Tree<FullNode<int>>* simpleTree = TreeConstructor(L"0-l1,0-r2,1-l3,1-r4,2-l5,2-r6");
     Tree<FullNode<int>>* deepTree = TreeConstructor(L"0-l1,0-r2,1-l3,2-r4,3-l5,4-r6,5-l7,6-l8,6-r9");
     Tree<FullNode<int>>* sparseTree = TreeConstructor(L"0-l1,0-r2,1-l3,1-r4,2-l5,2-r6,3-r7,4-l8,5-l9,6-r10");
 
     wprintf(L"PreOrder:\n");
-	PrintPreOrderRecursive(simpleTree->Root()); wprintf(L"\n");
+    PrintPreOrderRecursive(simpleTree->Root()); wprintf(L"\n");
     PrintPreOrderIterativeStack(simpleTree->Root()); wprintf(L"\n");
     PrintPreOrderParentPointer(simpleTree->Root()); wprintf(L"\n");
     PrintPreOrderRecursive(deepTree->Root()); wprintf(L"\n");
@@ -406,7 +427,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
     wprintf(L"BFS:\n");
     PrintBFSIterativeQueue(simpleTree->Root()); wprintf(L"\n");
-	PrintBFSLogLinear(simpleTree->Root()); wprintf(L"\n");
+    PrintBFSLogLinear(simpleTree->Root()); wprintf(L"\n");
     PrintBFSIterativeQueue(deepTree->Root()); wprintf(L"\n");
     PrintBFSLogLinear(deepTree->Root()); wprintf(L"\n");
     PrintBFSIterativeQueue(sparseTree->Root()); wprintf(L"\n");
@@ -417,8 +438,8 @@ int _tmain(int argc, _TCHAR* argv[])
     PrintZigZagTree(deepTree->Root()); wprintf(L"\n");
     PrintZigZagTree(sparseTree->Root()); wprintf(L"\n");
 
-	getchar();
+    getchar();
 
-	return 0;
+    return 0;
 }
 
