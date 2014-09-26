5a9081655062d3ac039aafedc6b19c5b47c9d0ea
Justin Rogers <justrog@gmail.com>
2014-09-26 00:31:03 -0700

    Variations on the Triangle problem for DP training.
    We have a recursive, top-down and bottom-up solution to show the differences.

--- DigiTec/InterviewQuestions/TrainingCourse/LeetCodeQuestions/Triangle.cpp
+++ DigiTec/InterviewQuestions/TrainingCourse/LeetCodeQuestions/Triangle.cpp
@@ -23,6 +23,34 @@ namespace Triangle
             }
             return intValues[0];
         }
+
+        int recurse(vector<vector<int>> &triangle, int row, int col)
+        {
+            if (row == (triangle.size() - 1))
+            {
+                return triangle[row][col];
+            }
+            return min(recurse(triangle, row + 1, col), recurse(triangle, row + 1, col + 1)) + triangle[row][col];
+        }
+
+        int topDown(vector<vector<int>> &triangle)
+        {
+            map<pair<int, int>, int> scratch;
+            return minPath(triangle, pair<int, int>( 0, 0 ), scratch);
+        }
+
+        int minPath(vector<vector<int>> &triangle, pair<int, int>& cell, map<pair<int, int>, int> &scratch)
+        {
+            if (cell.first == (triangle.size() - 1))
+            {
+                return triangle[cell.first][cell.second];
+            }
+            if (scratch.count(cell) == 0)
+            {
+                scratch[cell] = min(minPath(triangle, pair<int, int>(cell.first + 1, cell.second), scratch), minPath(triangle, pair<int, int>(cell.first + 1, cell.second + 1), scratch)) + triangle[cell.first][cell.second];
+            }
+            return scratch[cell];
+        }
     };
 
     void RunTests()
@@ -31,11 +59,13 @@ namespace Triangle
             vector < int > {2},
             vector < int > {3, 4},
             vector < int > {6, 5, 7},
-            vector < int > { 4, 1, 8, 3}
+            vector < int > {4, 1, 8, 3}
         };
 
         Solution s;
         int r1 = s.minimumTotal(input);
         _ASSERT(r1 == 11);
+        _ASSERT(r1 == s.topDown(input));
+        _ASSERT(r1 == s.recurse(input, 0, 0));
     }
 };
